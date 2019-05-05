c90b8707f4f0b7a8c75c3cb81a609b047c98aac7
az13js <1654602334@qq.com>
2019-05-05 22:44:04 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 cd src
 gcc -O3 circular_net_mnist.c mnist.c memory_file.c circular_train_data.c circular_net.c -lm -o mnist.out
-./mnist.out
+gcc -O3 circular_net_test.c circular_net.c -lm -o circular_net_test.out
+./circular_net_test.out
