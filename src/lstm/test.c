77eab332273e6dfb2fa43d78e41ab344c2ef2cad
az13js <1654602334@qq.com>
2019-05-06 21:52:47 +0800

    test

--- src/lstm/test.c
+++ src/lstm/test.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    struct lstmlib* lstm = lstmlib_create(0);
+    struct lstmlib* lstm = lstmlib_create(1000);
     printf("%s\n", lstmlib_get_error_message(lstm));
     lstmlib_destory(lstm);
     return 0;
